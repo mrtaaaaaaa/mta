f7f5205e6618ac3b7eb9887c4ad82ed8eaf74701
mohammadjavad <mohammadjavada78@gmail.com>
2023-11-20 09:47:23 +0330

    SliderLatestProduct comment

--- src/components/page/car-order/used/index.tsx
+++ src/components/page/car-order/used/index.tsx
@@ -5,13 +5,13 @@ import NormalVipPurchase from "../components/normal-vip-purchase";
 import { BannerCarUsedCarExchange, SliderUsedCarOrder } from "./components";
 import { SliderLatestProduct } from "@/attom/slider/latest-product/slider-latest-product";
 
-const UsedCarOrder = ({ usedData , brandModel}: any) => {
+const UsedCarOrder = ({ usedData, brandModel }: any) => {
   return (
     <div>
       <div className="my-4">
-        <NormalVipPurchase brandModel={brandModel}/>
+        <NormalVipPurchase brandModel={brandModel} />
       </div>
-      <SliderLatestProduct link="/used" data={usedData} />
+      {/* <SliderLatestProduct link="/used" data={usedData} /> */}
       <SliderUsedCarOrder />
       <div className="mt-20">
         <BannerCarUsedCarExchange />
